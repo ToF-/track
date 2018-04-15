3fba31465cd4152ca146f4c9e9ed837d944b2065
ToF <cthibaut@octo.com>
2018-04-15 22:49:00 +0200

    refcatoring

--- Specs.hs
+++ Specs.hs
@@ -7,7 +7,10 @@ main = hspec $ do
             let tl = newTaskList
             start "TOF" 4807 (2018,04,15,19,40) tl
              `shouldBe` Right [(4807,"TOF",Start,(2018,04,15,19,40))]
+
         it "should not allow a duplicate start" $ do
             let tl = start "TOF" 4807 (2018,04,15,19,40) newTaskList
             (tl >>= start "TOF" 4807 (2018,04,15,21,30))
              `shouldBe` Left "Task already started"
+
+        
--- Track.hs
+++ Track.hs
@@ -4,15 +4,17 @@ type TaskList = [TaskRecord]
 type TaskRecord = (TaskId,Person,Status,TimeStamp)
 type TaskId = Int
 type Person = String
-data Status = Start | InProgress | Waiting | Done
+data Status = Start | Done | Pause | Resume
     deriving (Eq, Ord, Show)
 type TimeStamp = (Int,Int,Int,Int,Int)
 type Message = String
 
 
 start :: Person -> TaskId -> TimeStamp -> TaskList -> Either Message TaskList 
-start p id ts tl | not (null (filter (\(id',_,_,_) -> id'== id) tl)) = Left "Task already started" 
-                 | otherwise                                         = Right $ (id,p,Start,ts):tl
+start p id ts tl | taskExist id tl = Left "Task already started" 
+                 | otherwise       = Right $ (id,p,Start,ts):tl
+    where
+    taskExist id tl = not $ null $ filter (\(i,_,_,_) -> id==i) tl 
 
 newTaskList :: TaskList
 newTaskList = []
